506da7bc07879002e6253532f5c0db38a0d8aa21
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-03 10:02:34 +0530

    add note on filter and outputs

--- basics.js
+++ basics.js
@@ -3,4 +3,19 @@
     and processing streams of data as they come in without using loops.
 */
 [1, 2, 3].forEach((x) => console.log(x));
+// outputs : 1 2 3
+
+/* 
+    Map : if you wanna transform every item in a collection and put it into a new collection then use map
+*/
 [1, 2, 3].map((x) => x + 1); // dont change original array, create whole new array
+// outputs : [2, 3, 4]
+
+/* 
+    Filter : apply a function to every item in the array, difference is this function is a test function,
+    instead of transforming the value its going to apply a test to every single value, and only if the 
+    value passes the test, will it make it to the new collection
+
+*/
+[1, 2, 3].filter((x) => x > 1);
+// outputs : [2, 3]
